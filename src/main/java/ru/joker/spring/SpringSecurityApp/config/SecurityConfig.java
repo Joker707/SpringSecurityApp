f02c4dfac6b6a52b9ccfa1fd3193091c9c8aa95c
joker <ser.fyodoroff@yandex.ru>
2023-05-05 02:27:05 +0300

    Create auth w/o AuthProvider

--- src/main/java/ru/joker/spring/SpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/joker/spring/SpringSecurityApp/config/SecurityConfig.java
@@ -3,20 +3,20 @@ package ru.joker.spring.SpringSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import ru.joker.spring.SpringSecurityApp.security.AuthProvider;
+import ru.joker.spring.SpringSecurityApp.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProvider authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProvider authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
     }
 
 }
